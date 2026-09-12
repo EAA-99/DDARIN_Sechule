1aa3026e57c7edf6bf5a71a042c818ca55676969
EAA-99 <kimminja129@gmail.com>
2026-09-12 23:58:52 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-﻿const CACHE_NAME = "ddarin-calendar-v565";
+﻿const CACHE_NAME = "ddarin-calendar-v566";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=565",
-  "./app.js?v=565",
-  "./games.js?v=565",
+  "./style.css?v=566",
+  "./app.js?v=566",
+  "./games.js?v=566",
   "./manifest.json",
   "./DDARIN_아바타.jpg",
   "./높은양갈래따린윙크.gif",
